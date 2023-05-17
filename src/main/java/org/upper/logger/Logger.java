3baf5afaa0d760055acdac73bd3a805ebfd3eab2
Gustavo Porto <gustavo@sumopay.org>
2023-05-17 14:10:38 -0300

    Added logger name and timestamp to the log message.

--- src/main/java/org/upper/logger/Logger.java
+++ src/main/java/org/upper/logger/Logger.java
@@ -1,9 +1,33 @@
 package org.upper.logger;
 
 
+import java.time.LocalDateTime;
+
 public class Logger {
 
+    private final String name;
+
+    public Logger() {
+        this("");
+    }
+
+    public Logger(String name) {
+        this.name = name == null ? "" : name;
+    }
+
+    public Logger(Class<?> clazz) {
+        this(clazz.getName());
+    }
+
     public void log(String message) {
-        System.out.println(message);
+        var finalMessage = createFinalMessage(message);
+        System.out.println(finalMessage);
+    }
+
+    String createFinalMessage(String message) {
+        return String.format("[%1$tF %1$tT] %2$s - %3$s",
+                LocalDateTime.now(),
+                name.isEmpty() ? "" : name,
+                message);
     }
 }
--- src/test/java/org/upper/logger/LoggerTest.java
+++ src/test/java/org/upper/logger/LoggerTest.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import static java.time.format.DateTimeFormatter.ofPattern;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -16,6 +21,7 @@ public class LoggerTest {
     static PrintStream printStream;
     Logger logger;
     String message = "any log message";
+    LocalDateTime dateTime = LocalDateTime.of(2023, 5, 17, 14, 9, 20);
 
     @BeforeAll
     public static void init() {
@@ -25,16 +31,22 @@ public class LoggerTest {
 
     @BeforeEach
     public void setUp() {
-        this.logger = new Logger();
+        this.logger = new Logger(this.getClass());
     }
 
     @Test
     public final void testLog() {
 
-        logger.log(message);
+        try (var localDateTimeMock = mockStatic(LocalDateTime.class)) {
+            localDateTimeMock.when(LocalDateTime::now).thenReturn(dateTime);
 
-        verify(printStream).println(message);
+            logger.log(message);
 
-        verifyNoMoreInteractions(printStream);
+            var expectedMessage = "[2023-05-17 14:09:20] org.upper.logger.LoggerTest - any log message";
+
+            verify(printStream).println(expectedMessage);
+
+            verifyNoMoreInteractions(printStream);
+        }
     }
 }
